99e9bd3ac6bfce463f444c6e268521b0f608f702
htetaungkhant <htetaung23.ha@gmail.com>
2023-07-13 10:14:39 +0630

    generate resource for messages and websocket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppGateway } from './app/app.gateway';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService],
 })
 export class AppModule {}
